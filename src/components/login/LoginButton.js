94fed5e44819e9d9d03cf68c9fdf84e2bd754f91
BM <jmspon33@gmail.com>
2025-03-12 22:40:44 +0900

    useLoginStateHook 수정 (jwt토큰 만료, 유저정보 추가 )

--- src/components/login/LoginButton.js
+++ src/components/login/LoginButton.js
@@ -2,31 +2,16 @@
 
 import useLoginStateHook from "@/customHook/useLoginStateHook";
 import GoogleLoginButton from "@/components/login/googleLoginButton/GoogleLoginButton";
-import {gql, useMutation, useQuery} from "@apollo/client";
-
-const GET_LOGIN_USER_INFO = gql`
-query LoginState {
-    loginState {
-        email
-        uuid
-    }
-}
-`
 
 export default function LoginButton() {
-    const [isLogin, logoutEv] = useLoginStateHook()
-    const {loading, error, data} = useQuery(GET_LOGIN_USER_INFO)
+    const {loginUser, logout} = useLoginStateHook()
 
-    if (!isLogin) return <GoogleLoginButton/>;
-
-    if (loading) return null;
-
-    const {email, uuid} = data.loginState
+    if (!loginUser) return <GoogleLoginButton/>;
 
     return <>
         <div className="flex gap-4 items-center">
-            <p>{email}</p>
-            <button onClick={logoutEv}>
+            <p>{loginUser.email}</p>
+            <button onClick={logout}>
                 Logout
             </button>
         </div>
--- src/customHook/useLoginStateHook.js
+++ src/customHook/useLoginStateHook.js
@@ -1,15 +1,50 @@
 "use client"
 
 import {useEffect, useState} from "react";
+import {gql, useLazyQuery, useQuery} from "@apollo/client";
 
+const GET_LOGIN_USER_INFO = gql`
+query LoginState {
+    loginState {
+        email
+        uuid
+    }
+}
+`
+/**
+ * 로그인 정보 조회
+ *
+ * @returns {{ loginUser: { email: string, uuid: string } | undefined | null, logout: () => void }}
+ * */
 export default function useLoginStateHook() {
-    const [hasJwt, setHasJwt] = useState(undefined);
+    const [jwt, setJwt] = useState(undefined);
+    const [loginUser, setLoginUser] = useState(undefined);
 
+    const [getLoginUser, {loading, error, data}] = useLazyQuery(GET_LOGIN_USER_INFO)
+
+    // 컴포넌트 마운트 시 Jwt 쿠키 정보 로드
     useEffect(() => {
-        setHasJwt(!!getJwtByCookie());
+        setJwt(getJwtByCookie())
     }, []);
 
-    return [hasJwt, logoutEv];
+    // 쿠키기반 인증정보 조회
+    useEffect(() => {
+        if (!jwt) {
+            return;
+        }
+        getLoginUser()
+            .then(({data: {loginState}}) => {
+                // 로그인 정보 x -> Jwt 쿠키 파기 및 새로고침
+                if (!loginState) {
+                    logoutEv()
+                }
+                setLoginUser(loginState)
+            })
+
+
+    }, [jwt]);
+
+    return {loginUser, logout};
 }
 
 function getJwtByCookie() {
@@ -26,7 +61,7 @@ function getJwtByCookie() {
     return jwtCookie[1] || null;
 }
 
-function logoutEv() {
+function logout() {
     document.cookie = "JWT_TOKEN=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     window.location.reload();
 }
