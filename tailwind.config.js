c50bac1e6007eb1ff5eb0132eb11452b32f65430
BmSrv <jmspon33@gmail.com>
2025-03-13 23:56:33 +0900

    tailwind 플러그인 추가 (tailwind-scrollbar-hiden)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import scrollbarHide from 'tailwind-scrollbar-hide'
+
 module.exports = {
   content: [],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [scrollbarHide],
 }
-
